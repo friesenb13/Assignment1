857cef1d4d2ffa53eaccf9dbae4352f288850a67
Becky <77301055+friesenb13@users.noreply.github.com>
2022-10-25 17:19:56 -0600

    integrating code pen magic

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -3,97 +3,13 @@ var app = new Framework7({
   el: '#app',
   // ... other parameters
 });
-var mainView = app.views.create('.view-main')
+// var mainView = app.views.create('.view-main')
 
 
-//water level thresholds
-var waterlevel = 20; //starting level
-var nowater = 0;
-var drowning = 40;
-//nutrient levels
-
-var nutrientlevel= 30;
-var nonutrient=0;
-var full=40;
-
-//trim
-// var neardeath =false;
-var trimmed=false;
-
-var neardeath = false; //plant starts healthy
-
-function dryout(){
-  waterlevel --; //every time the function runs, the water level decreases by 1
-  console.log(waterlevel);
-  checkhealth();
-  var watertimer = setTimeout(dryout,500);
-}
-dryout();
-
-
-
-function nutrients(){
-  nutrientlevel --; //over time it will go down
-  console.log(nutrientlevel)
-  checkhealth();
-  var foodtimer= setTimeout(nutrients,500);
-}
-nutrients();
-
-function checkhealth(){
-  if(waterlevel <= nowater || waterlevel >=drowning || nutrientlevel <= nonutrient){
-    neardeath=true;
-    console.log("help!")
-    $("#plant path").css("fill", "chocolate")
-  } 
-
-  if (neardeath && waterlevel > nowater && nutrientlevel > nonutrient){
-    $("#plant path").css("fill", "#568B62")
-    neardeath=false;
-    setTimeout(function(){
-      $("#trim").fadeIn();
-      trimmed=false;
-    }, 5000)
-  }
-  
-}
-
-//event listeners
-
-$("#water-me").on("click", function() { //anonymous callback function
-
-  waterlevel +=20;
-  $("#water").fadeIn().delay(3000).fadeOut();
-  
-
-})
-$("#feed-me").on("click", function() { //anonymous callback function
-
-  nutrientlevel +=20;
-  $("#food").fadeIn().delay(3000).fadeOut();
-  
-
-})
-
-//trim
-$("#trim-me").on("click", function(){
-  trimmed=true;
-  $("#scissors").fadeIn().delay(2000).fadeOut();
-  $("#trim").fadeOut();
-})
-
-
-// by default:
-// - plant starts healthy
-// - dry out over time
-// - deplete in nutrients over time
-
-// interactions:
-// - water it, replenish the plant
-// - feed it, nutrients for the plant
-// - trim it
-
-//care:
-// - too much water, plant near death
-// - too much fertilizer, plant near death
-// - if plant falls into near death state, you can only heal it by trimming
+$(document).ready(function() {
+  var $magic = $(".magic"),
+      magicWHalf = $magic.width() / 2;
+  $(document).on("mousemove", function(e) {
+    $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+  });
+});
