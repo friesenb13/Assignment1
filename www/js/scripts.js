dbae40873a137e9ca33593fa57f5a5a59cedf045
friesenb13 <friesenb@uleth.ca>
2022-10-27 18:32:16 -0600

    add pages

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,9 +1,27 @@
 var app = new Framework7({
   // App root element
+  
   el: '#app',
   // ... other parameters
+  routes: [
+    {
+      path: '/',
+      url: 'index.html',
+    },
+    {
+      path: '/page2/',
+      url: 'pages/page2.html',
+    },
+    {
+      path: '/page3/',
+      url: 'pages/page3.html',
+    },
+  ],
+  
+  
 });
-// var mainView = app.views.create('.view-main')
+var mainView = app.views.create('.view-main')
+
 
 //code pen
 $(document).ready(function() {
@@ -18,15 +36,15 @@ $(document).ready(function() {
 // Popover
 // export default (props, { $, $f7, $on }) => {
 
-  $on('pageInit', () => {
-    // DOM events for About popover
-    $('.popover-about').on('popover:open', function (e) {
-      console.log('About popover open');
-    });
-    $('.popover-about').on('popover:opened', function (e) {
-      console.log('About popover opened');
-    });
-  })
+  // $on('pageInit', () => {
+  //   // DOM events for About popover
+  //   $('.popover-about').on('popover:open', function (e) {
+  //     console.log('About popover open');
+  //   });
+  //   $('.popover-about').on('popover:opened', function (e) {
+  //     console.log('About popover opened');
+  //   });
+  // })
 
   // return $render;
 // }
