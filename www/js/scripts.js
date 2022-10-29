8adbf375b92c43645819da58ae08e3be165012f3
friesenb13 <friesenb@uleth.ca>
2022-10-28 19:49:34 -0600

    trying to fix things but not getting very far

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -22,29 +22,18 @@ var app = new Framework7({
 });
 var mainView = app.views.create('.view-main')
 
+$("#turt").on("click", function(){
+  console.log("turt");
+  // $("#bubble").fadeIn();
+})
+
+
 
 //code pen
-$(document).ready(function() {
-  var $magic = $(".magic"),
-      magicWHalf = $magic.width() / 2;
-  $(document).on("mousemove", function(e) {
-    $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
-  });
-});
-
-
-// Popover
-// export default (props, { $, $f7, $on }) => {
-
-  // $on('pageInit', () => {
-  //   // DOM events for About popover
-  //   $('.popover-about').on('popover:open', function (e) {
-  //     console.log('About popover open');
-  //   });
-  //   $('.popover-about').on('popover:opened', function (e) {
-  //     console.log('About popover opened');
-  //   });
-  // })
-
-  // return $render;
-// }
+// $(document).ready(function() {
+//   var $magic = $(".magic"),
+//       magicWHalf = $magic.width() / 2;
+//   $(document).on("mousemove", function(e) {
+//     $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+//   });
+// });
