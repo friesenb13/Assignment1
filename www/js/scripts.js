990ae5a43d139ee860e5644d5f97d443b8b3ed88
friesenb13 <friesenb@uleth.ca>
2022-10-30 15:57:28 -0600

    reworking layout

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -22,18 +22,18 @@ var app = new Framework7({
 });
 var mainView = app.views.create('.view-main')
 
-$("#turt").on("click", function(){
-  console.log("turt");
-  // $("#bubble").fadeIn();
+$("#turt").on("click", function() { //anonymous callback function
+
+  console.log("bubble")
+  
+
 })
 
-
-
-//code pen
-// $(document).ready(function() {
-//   var $magic = $(".magic"),
-//       magicWHalf = $magic.width() / 2;
-//   $(document).on("mousemove", function(e) {
-//     $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
-//   });
-// });
+//quotes array
+//when panel opens it displays a new quote everytime
+$(".panel").on("panel:open", function(){
+  var motivation = ["Just keep swimming", "I will find comfort in the rhythm of the sea.", "A fish will never realize its potential until you throw it in water.", "Never forget that only dead fish swim with the stream"];
+var indexNum = Math.floor(Math.random()*motivation.length);
+console.log(indexNum);
+$("#quote").text(motivation[indexNum])
+})
